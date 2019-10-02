abd9bb51946997c89ba0a6fb2d56c9f7867d1434
gregmanthey <gmanthey03@gmail.com>
2019-10-02 12:38:28 -0500

    Added dropdown for selecting Role in Register page. Added base model for Customer and Employee.

--- TrashCollector/Controllers/AccountController.cs
+++ TrashCollector/Controllers/AccountController.cs
@@ -17,9 +17,11 @@ namespace TrashCollector.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
+        ApplicationDbContext context;
 
         public AccountController()
         {
+            context = new ApplicationDbContext();
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
@@ -139,6 +141,7 @@ namespace TrashCollector.Controllers
         [AllowAnonymous]
         public ActionResult Register()
         {
+            ViewBag.Name = new SelectList(context.Roles.ToList(), "Name", "Name");
             return View();
         }
 
@@ -156,13 +159,23 @@ namespace TrashCollector.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                    await this.UserManager.AddToRoleAsync(user.Id, model.UserRoles);
+                    switch (model.UserRoles)
+                    {
+                        case "Employee":
 
+                            break;
+                        case "Customer":
+                            break;
+                        default:
+                            break;
+                    }
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZIP { get; set; }
+        public double AccountBalance { get; set; }
+    }
+}
