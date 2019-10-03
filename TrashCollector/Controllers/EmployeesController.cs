8613529a8558e29ec5a013e927cc7d79f4cd39d5
gregmanthey <gmanthey03@gmail.com>
2019-10-03 08:59:39 -0500

    Created EmployeesController and copied logic from CustomersController (removing excess fields from POST Edit and replacing all instances of Customer/customer with Employee/employee)

---
+++ TrashCollector/Controllers/EmployeesController.cs
@@ -0,0 +1,112 @@
+﻿using System;Customer
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrashCollector.Models;
+
+namespace TrashCollector.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        ApplicationDbContext context;
+        public EmployeesController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(context.Employees.ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int id)
+        {
+            var employeeFoundInDb = GetEmployeeFromId(id);
+            return View(employeeFoundInDb);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            //Employee employee = new Employee();
+            return View(new Employee());
+        }
+
+        // POST: Employees/Create
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.Employees.Add(employee);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(GetEmployeeFromId(id));
+        }
+
+        // POST: Employees/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Employee employee)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var employeeFoundInDb = GetEmployeeFromId(id);
+                employeeFoundInDb.FirstName = employee.FirstName;
+                employeeFoundInDb.LastName = employee.LastName;
+                employeeFoundInDb.ZIP = employee.ZIP;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(GetEmployeeFromId(id));
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Employee employee)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var employeeFoundInDb = GetEmployeeFromId(id);
+                context.Employees.Remove(employeeFoundInDb);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        private Employee GetEmployeeFromId(int id)
+        {
+            return context.Employees.Find(id);
+        }
+        private ApplicationUser GetUserFromGuid(string userGuid)
+        {
+            return context.Users.Find(userGuid);
+        }
+    }
+}
